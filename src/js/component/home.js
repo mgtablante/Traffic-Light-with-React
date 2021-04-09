13b9413a86c47df9e27b91124ed856a6eb1ffea9
mgtablante <mgtablantel@gmail.com>
2021-04-09 21:41:51 +0000

    falta funcionalidad

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,49 +1,51 @@
 import React, { useState } from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import { useState, useEffect } from "react";
 
 //create your first component
 export function Home() {
-    //const [color, setColor] = useState("red");
-    const[btnClass, setBtnClass] = useState(""),
-    const[btnClassYellow, setBtnClassYellow] = useState(""),
-    const[btnClassGreen, setBtnClassGreen] = useState(""),
+	//const [color, setColor] = useState("red");
+	const [btnClass, setBtnClass] = useState("");
+	const [btnClassYellow, setBtnClassYellow] = useState("");
+	const [btnClassGreen, setBtnClassGreen] = useState("");
 
-    //Red
-    const handleClick = () => [
-        setBtnClass("selected");
-        setBtnClassYellow("");
-        setBtnClassGreen("");
-    ];
-    //Yellow
-       const handleClickYellow = () => [
-        setBtnClass("");
-        setBtnClassYellow("selected");
-        setBtnClassGreen("");
-    ];
-    //Green
-       const handleClickGreen = () => [
-        setBtnClass("");
-        setBtnClassYellow("");
-        setBtnClassGreen("selected");
-    ];
+	//Red
+	const handleClick = () => [
+		setBtnClass("selected"),
+		setBtnClassYellow(""),
+		setBtnClassGreen("")
+	];
+	//Yellow
+	const handleClickYellow = () => [
+		setBtnClass(""),
+		setBtnClassYellow("selected"),
+		setBtnClassGreen("")
+	];
+	//Green
+	const handleClickGreen = () => [
+		setBtnClass(""),
+		setBtnClassYellow(""),
+		setBtnClassGreen("selected")
+	];
 	return (
 		<div>
 			<div className="traffic"></div>;<h1>Traffic Light with React</h1>
 			<div className="container">
-				<span
-					className={`lightred ${btnClass}`}/>
-                    onClick={handleClick}git 
-                <br />
-				<span
-					className={`lightyellow ${btnClassYellow}`}/>
-                    onClick={handleClickYellow}
-
+				<span className={`lightred ${btnClass}`} />
+				onClick
+				{() => {
+					handleClick;
+				}}
 				<br />
-				<span
-					className={`lightgreen ${btnClassGreen}`}/>
-                    onClick={handleClickGreen}
-
+				<span className={`lightyellow ${btnClassYellow}`} />
+				onClick
+				{() => {
+					handleClickYellow;
+				}}
+				<br />
+				<span className={`lightgreen ${btnClassGreen}`} />
+				onClick
+				{() => {
+					handleClickGreen;
+				}}
 			</div>
 		</div>
 	);
